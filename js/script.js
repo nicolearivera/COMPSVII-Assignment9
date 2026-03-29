110a5b83eb0125940723f470ff27ba7fa7ba974e
nicolearivera <narivera@cedarcrest.edu>
2026-03-29 19:10:14 -0400

    Added another url giphy

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 console.log("script.js loaded");
 https://api.giphy.com/v1/gifs/search?api_key=8jQKgqbdWUZCb4v6BrFoCddidOSw1dau&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+https://api.giphy.com/v1/gifs/search?api_key=8jQKgqbdWUZCb4v6BrFoCddidOSw1dau&q=anime&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
