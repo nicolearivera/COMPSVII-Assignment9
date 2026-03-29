0267bcb1b0ddca9c680b6c89c77617cb2af114a6
nicolearivera <narivera@cedarcrest.edu>
2026-03-29 19:37:16 -0400

    It works!!!!!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("script.js loaded");
-# https://api.giphy.com/v1/gifs/search?api_key=8jQKgqbdWUZCb4v6BrFoCddidOSw1dau&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
-# https://api.giphy.com/v1/gifs/search?api_key=8jQKgqbdWUZCb4v6BrFoCddidOSw1dau&q=anime&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+// https://api.giphy.com/v1/gifs/search?api_key=8jQKgqbdWUZCb4v6BrFoCddidOSw1dau&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+// https://api.giphy.com/v1/gifs/search?api_key=8jQKgqbdWUZCb4v6BrFoCddidOSw1dau&q=anime&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
 
 document.addEventListener("DOMContentLoaded", () => {
   let images = [];
